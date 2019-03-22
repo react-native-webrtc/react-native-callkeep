87783b78232a2db78b7e549fb46e95d535867a01
bob <bob@mediot.com.tw>
2019-03-22 18:17:28 +0800

    Removed garbage code

--- android/src/main/java/io/wazo/callkeep/RNCallKeepModule.java
+++ android/src/main/java/io/wazo/callkeep/RNCallKeepModule.java
@@ -49,8 +49,6 @@ import com.facebook.react.modules.core.DeviceEventManagerModule.RCTDeviceEventEm
 
 // @see https://github.com/kbagchiGWC/voice-quickstart-android/blob/9a2aff7fbe0d0a5ae9457b48e9ad408740dfb968/exampleConnectionService/src/main/java/com/twilio/voice/examples/connectionservice/VoiceConnectionServiceActivity.java
 public class RNCallKeepModule extends ReactContextBaseJavaModule {
-    Activity mActivity = null;
-
     public static final int REQUEST_READ_PHONE_STATE = 1337;
     public static final int REQUEST_REGISTER_CALL_PROVIDER = 394859;
 
@@ -238,10 +236,8 @@ public class RNCallKeepModule extends ReactContextBaseJavaModule {
 
         focusIntent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
 
-        final Activity activity = getActivity();
-        if (activity != null) {
-            activity.startActivity(focusIntent);
-        }
+        Activity activity = getCurrentActivity();
+        activity.startActivity(focusIntent);
     }
 
     private void registerPhoneAccount(Context appContext) {
@@ -310,13 +306,6 @@ public class RNCallKeepModule extends ReactContextBaseJavaModule {
 
     private Context getAppContext() {
         return this.reactContext.getApplicationContext();
-    }
-
-    private Activity getActivity() {
-        Activity activity = getCurrentActivity();
-        if (activity == null) activity = mActivity;
-
-        return activity;
     }
 
     private class VoiceBroadcastReceiver extends BroadcastReceiver {
