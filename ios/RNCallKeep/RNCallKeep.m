bef6bbcc30ff720252588568cf71316ebbde3c6c
Tuan Luong <tuanluong.it@gmail.com>
2019-08-17 14:07:44 +0700

    delay 3.0 seconds when end call to make sure app have enough time to make connection to update call state

--- ios/RNCallKeep/RNCallKeep.m
+++ ios/RNCallKeep/RNCallKeep.m
@@ -510,7 +510,11 @@ RCT_EXPORT_METHOD(reportUpdatedCall:(NSString *)uuidString contactIdentifier:(NS
 #endif
     NSString *callUUID = [self containsLowerCaseLetter:action.callUUID.UUIDString] ? action.callUUID.UUIDString : [action.callUUID.UUIDString lowercaseString];
     [self sendEventWithName:RNCallKeepPerformEndCallAction body:@{ @"callUUID": callUUID }];
-    [action fulfill];
+    double delayInSeconds = 3.0;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+      [action fulfill];
+    });
 }
 
 -(void)provider:(CXProvider *)provider performSetHeldCallAction:(CXSetHeldCallAction *)action
