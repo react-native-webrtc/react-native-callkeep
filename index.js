4fb7d0879c5e995732324b04c4858b3b86c9076c
Andre Nozari <ajnozari@ajnoz.com>
2020-04-30 04:56:28 -0400

    Using proper variable >_>

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ class RNCallKeep {
     }
   };
 
-  isCallActive = (uuid) => RNCallKeepModule.isCallActive(uuid);
+  isCallActive = async(uuid) => RNCallKeepModule.isCallActive(uuid);
 
   endCall = (uuid) => RNCallKeepModule.endCall(uuid);
 
