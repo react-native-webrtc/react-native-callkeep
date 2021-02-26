6e98a84bc997aa4624afb9be379b7f1594e773d6
Filipe Natanael <filipenatanael@live.com>
2021-02-26 06:28:33 -0300

    feat: enable calling account preference activity directly

--- android/src/main/java/io/wazo/callkeep/RNCallKeepModule.java
+++ android/src/main/java/io/wazo/callkeep/RNCallKeepModule.java
@@ -498,9 +498,10 @@ public class RNCallKeepModule extends ReactContextBaseJavaModule {
         if (!isConnectionServiceAvailable()) {
             return;
         }
-
-        // TODO: Add support for main android devices in order to open select account directly
-        if (Build.MANUFACTURER.equalsIgnoreCase("Samsung")) {
+        
+        // Go to <EnableAccountPreferenceActivity> screen directly, please notice that's depends of the manufacturer
+        Log.d("Callkeep", "Android manufacturer: " + Build.MANUFACTURER);
+        if (Build.MANUFACTURER.equalsIgnoreCase("Samsung") || Build.MANUFACTURER.equalsIgnoreCase("motorola")) {
             Intent intent = new Intent();
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
             intent.setComponent(new ComponentName("com.android.server.telecom",
