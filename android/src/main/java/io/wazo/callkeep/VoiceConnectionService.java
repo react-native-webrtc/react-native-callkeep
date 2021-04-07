a2801f366008ffc1bc41676f53b5f0072a0f00cb
german970814 <german.alzate970814@gmail.com>
2021-04-07 16:55:47 -0500

    Set mode to video bidirectional

--- android/src/main/java/io/wazo/callkeep/VoiceConnectionService.java
+++ android/src/main/java/io/wazo/callkeep/VoiceConnectionService.java
@@ -56,6 +56,7 @@ import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+import android.telecom.VideoProfile;
 import static io.wazo.callkeep.Constants.ACTION_AUDIO_SESSION;
 import static io.wazo.callkeep.Constants.ACTION_ONGOING_CALL;
 import static io.wazo.callkeep.Constants.ACTION_CHECK_REACHABILITY;
@@ -206,6 +207,7 @@ public class VoiceConnectionService extends ConnectionService {
         outgoingCallConnection = createConnection(request);
         outgoingCallConnection.setDialing();
         outgoingCallConnection.setAudioModeIsVoip(true);
+        outgoingCallConnection.setVideoState(VideoProfile.STATE_BIDIRECTIONAL);
         outgoingCallConnection.setCallerDisplayName(displayName, TelecomManager.PRESENTATION_ALLOWED);
 
         startForegroundService();
