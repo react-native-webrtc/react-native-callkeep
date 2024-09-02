0bb7b97501da7493bc6bc850106212594f041a03
Adaware Nero <neroadaware@gmail.com>
2024-09-02 14:11:57 +0100

    catch errros when starting foreground service for microphone

--- android/src/main/java/io/wazo/callkeep/VoiceConnectionService.java
+++ android/src/main/java/io/wazo/callkeep/VoiceConnectionService.java
@@ -320,7 +320,11 @@ public class VoiceConnectionService extends ConnectionService {
         Log.d(TAG, "[VoiceConnectionService] Starting foreground service");
 
         Notification notification = notificationBuilder.build();
-        startForeground(FOREGROUND_SERVICE_TYPE_MICROPHONE, notification);
+        try {
+            startForeground(FOREGROUND_SERVICE_TYPE_MICROPHONE, notification);
+        } catch (Exception e) {
+            Log.w(TAG, "[VoiceConnectionService] Can't start foreground service : " + e.toString());
+        }
     }
 
     private void stopForegroundService() {
@@ -331,7 +335,11 @@ public class VoiceConnectionService extends ConnectionService {
             Log.d(TAG, "[VoiceConnectionService] Discarding stop foreground service, no service configured");
             return;
         }
-        stopForeground(FOREGROUND_SERVICE_TYPE_MICROPHONE);
+         try {
+            stopForeground(FOREGROUND_SERVICE_TYPE_MICROPHONE);
+        } catch (Exception e) {
+            Log.w(TAG, "[VoiceConnectionService] can't stop foreground service :" + e.toString());
+        }
     }
 
     private void wakeUpApplication(String uuid, String number, String displayName) {
